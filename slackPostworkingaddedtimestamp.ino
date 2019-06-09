a2a95584a6353ce85032d4ab36556ff215f15cf2
U-DESKTOP-VCQNK3I\Skyler <skytrobbins@gmail.com>
2019-06-09 17:56:32 -0600

    V1.0: currently sketch on door bot

--- slackPostworkingaddedtimestamp.ino
+++ slackPostworkingaddedtimestamp.ino
@@ -3,6 +3,9 @@
 #include <ArduinoOTA.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <EEPROM.h>
+
+int DoorStatusAddress = 2;
 /*
  WIFI CONFIGURATION
  */
@@ -14,23 +17,25 @@ char pwd[] = "hunterisbusted";
  */
 const String slack_hook_url = "https://hooks.slack.com/services/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";
 const String slack_emoji = "heart";
-const String slack_message = "hey world";
+const String ToiletInUseMessage = "Bathroom is currently occupied as of: ";
+const String ToiletIsFree = "Bathroom has become vacant as of: ";
+float SleepTime = 10e6; // time in microseconds
+
 
 
 WiFiUDP ntpUDP;
 const String slack_username = "tayjojo";
 int AnalogData;
 bool isBathroomInUse = false;
+int DoorReadPin = 5; //D1
+
 // You can specify the time server pool and the offset (in seconds, can be
 // changed later with setTimeOffset() ). Additionaly you can specify the
 // update interval (in milliseconds, can be changed using setUpdateInterval() ).
-NTPClient timeClient(ntpUDP, "2.us.pool.ntp.org", 21600, 1000); //21600 is the offset
-void setup()
-{
- // pinMode(ledPin, OUTPUT);
- // pinMode(buttonPin, INPUT_PULLUP);
-  Serial.begin(115200);
-  Serial.println();
+NTPClient timeClient(ntpUDP, "2.us.pool.ntp.org", 21600); //21600 is the offset, add another argument at end to set update interval
+void connect() {
+
+  
 
   WiFi.begin(SSID, pwd);
   timeClient.begin();
@@ -65,7 +70,6 @@ void setup()
   });
   ArduinoOTA.begin();
 }
-
 
 
 bool postMessageToSlack(String msg)
@@ -106,35 +110,90 @@ bool postMessageToSlack(String msg)
     return false;
   }
 }
+void loop() {
+  
+}
 
-void loop()
+bool handleDoorHasOpened() {
+  bool isSuccessful;
+  timeClient.update(); //get current time
+  String CombinedVacant = (ToiletIsFree + timeClient.getFormattedTime()); // concatenate vacant message with timestamp
+  Serial.println("door has opened");
+  Serial.println(timeClient.getFormattedTime());
+  isSuccessful = postMessageToSlack(CombinedVacant); // send message to slack
+  Serial.println("toilet is free");
+  writeDoorStatusToEEPROM(DoorStatusAddress, false); // store new door status (door is open)
 
-{
-  const String ToiletInUseMessage = "Bathroom is currently occupied as of: ";
-  const String ToiletIsFree = "Bathroom has become vacant as of: ";
-  bool requestsuccess;
-  AnalogData = analogRead(0);
+  return isSuccessful;
+}
 
-  if(AnalogData > 512 && isBathroomInUse) {
-    String CombinedVacant = (ToiletIsFree + timeClient.getFormattedTime());
-      timeClient.update();
+bool handleDoorHasClosed() {
+  bool isSuccessful;
+  timeClient.update(); //get current time
+  String CombinedOccupied = (ToiletInUseMessage + timeClient.getFormattedTime()); // concatenate occupied message with timestamp
+  Serial.println("door has closed");
+  Serial.println(timeClient.getFormattedTime());
+  isSuccessful = postMessageToSlack(CombinedOccupied); // send message to slack
+  writeDoorStatusToEEPROM(DoorStatusAddress, true); // store new door status (door is open)
+  
+  return isSuccessful;
+}
 
-     Serial.println(timeClient.getFormattedTime());
+void writeDoorStatusToEEPROM(int address, bool doorstatus) {
+  EEPROM.write(address, doorstatus);
+  EEPROM.commit();
+}
 
-    postMessageToSlack(CombinedVacant);
-    requestsuccess = Serial.println("toilet is free");
-    isBathroomInUse = false;
-  } else if (AnalogData < 512 && !isBathroomInUse) {
-    String combinedInUse = (ToiletInUseMessage + timeClient.getFormattedTime());
-    requestsuccess = postMessageToSlack(combinedInUse);
-    isBathroomInUse = true;
-    Serial.println("is in use");
+
+
+
+void setup() {
+  EEPROM.begin(4);
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
+  pinMode(DoorReadPin, INPUT);
+  //0 is open
+  //1 is closed
+
+  //when the door is closed, rst and wake will be connected and it will wake the device
+  //when the device is awake perform these actions:
+  //-1. make sure it's not after 6pm
+  //0. read EEPROM to see if door status has changed
+  //1. read pin 0 and see if the door is opened or closed
+  //2. send message to slack if the digitalRead returns HIGH
+  //3. go to sleep for 30 seconds
+  //4. upon waking up again, check the status of digitalRead, if it is LOW, send message to slack and go to sleep
+ 
+  bool PreviousDoorStatus = EEPROM.read(DoorStatusAddress);
+ // bool DigitalReading = digitalRead(DoorReadPin);
+  bool CurrentDoorStatus = digitalRead(DoorReadPin);
+  
+  bool isSuccessful;
+
+  Serial.print("EEPROM has begun, door status is: ");
+  Serial.println(PreviousDoorStatus);
+  Serial.print("Current door status is: ");
+  Serial.println(CurrentDoorStatus);
+
+  if(PreviousDoorStatus == CurrentDoorStatus) { // if there has been no change to the door
+    Serial.println("no change in door status");
+    ESP.deepSleep(SleepTime);
+  } else if(CurrentDoorStatus < PreviousDoorStatus) { // if door has been opened
+    Serial.println("Door seems to have opened");
+    connect();
+    isSuccessful = handleDoorHasOpened();
+    ESP.deepSleep(SleepTime);
+  } else if(CurrentDoorStatus > PreviousDoorStatus) { // if door has been closed
+    Serial.println("door seems to have closed");
+    connect();
+    isSuccessful = handleDoorHasClosed();
+    ESP.deepSleep(SleepTime);
   }
-  Serial.println(requestsuccess);
+  Serial.println("is successful? " + isSuccessful);
+  // Serial.println(requestsuccess);
   // Start handling OTA updates
-  ArduinoOTA.handle();
-  Serial.println(AnalogData);
+  // ArduinoOTA.handle();
+  //Serial.println(AnalogData);
 
-  //digitalWrite(ledPin, LOW);
-  delay(2000);
+  // delay(2000);
 }
